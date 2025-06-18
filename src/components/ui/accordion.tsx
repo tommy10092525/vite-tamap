16d06faaa63407261e082a46217a0bb0c93598d0
tommy10092525 <tommyxla777@gmail.com>
2025-06-18 14:18:17 +0900

    アコーディオンのアニメーションを削除

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -53,7 +53,7 @@ function AccordionContent({
   return (
     <AccordionPrimitive.Content
       data-slot="accordion-content"
-      className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+      className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down !duration-0"
       {...props}
     >
       <div className={cn("pt-0 pb-4", className)}>{children}</div>
