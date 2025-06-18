eb260328bfa3440dff6177c920dbe3f0f2cbb66b
tommy10092525 <tommyxla777@gmail.com>
2025-06-18 14:39:10 +0900

    デザインの調整

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,7 +3,7 @@ type CardProps={children:React.ReactNode,className?:string}
 const card = ({children,className}:CardProps) => {
   return (
     <div className={`
-      bg-white/20 border border-t-white/20 border-l-white/20 border-b-white/5 border-r-white/5 dark:bg-black/30 dark:bg-opacity-30 dark:border-t-white/30 dark:border-l-white/30 dark:border-b-white/10 dark:border-r-white/10
+      bg-white/5 border border-t-white/20 border-l-white/20 border-b-white/5 border-r-white/5 dark:bg-black/30 dark:bg-opacity-30 dark:border-t-white/30 dark:border-l-white/30 dark:border-b-white/10 dark:border-r-white/10
       shadow-lg p-2 rounded-2xl ${className}`}>
       {children}
     </div>
