c1d0b5ab55d5f2600ea20e4ed961830903bfc020
tommy10092525 <tommyxla777@gmail.com>
2025-06-20 01:30:24 +0900

    主要駅から西八王子駅南入口を削除

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,7 +13,7 @@ const stationNames = {
 
 const timetableApi="api"
 const holidayDataAPi="https://holidays-jp.github.io/api/v1/date.json"
-const majorStations=["相原駅西口","法政大学（町田市）","橋本駅北口","法政大学","めじろ台駅","西八王子駅南口","八王子駅南口","西八王子駅南入口"]
+const majorStations=["相原駅西口","法政大学（町田市）","橋本駅北口","法政大学","めじろ台駅","西八王子駅南口","八王子駅南口"]
 
 export {buildings,stationNames,timetableApi,holidayDataAPi,majorStations
 }
