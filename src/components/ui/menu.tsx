e435dbca81d29b93384dbb9f994e880c6f3226c9
tommy10092525 <tommyxla777@gmail.com>
2025-06-20 01:29:38 +0900

    テーマ切り替えボタンのスタイルの変更

--- src/components/ui/menu.tsx
+++ src/components/ui/menu.tsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from './sheet';
 import { Link } from 'react-router-dom';
-import { Menu as MenuImg, Send, Share, Info, Instagram, SunMoon } from "lucide-react"
+import { Menu as MenuImg, Send, Share, Info, Instagram, Sun, Moon } from "lucide-react"
 import { useTheme } from "../theme-provider"
 
 const Menu = () => {
@@ -15,12 +15,13 @@ const Menu = () => {
         </SheetTrigger>
         <SheetContent className="bg-black/30 dark:bg-zinc-950/30 border-l border-l-gray-600">
           <SheetTitle className="shadow-xl mt-3 text-white text-xl text-center">Menu</SheetTitle>
-          <div className='flex bg-gray-900 dark:bg-zinc-950/80 shadow-xl mx-10 p-2 border border-zinc-700 rounded-lg text-white text-center hover:underline will-change-auto' onClick={() => {
+          <button className='flex bg-gray-900 dark:bg-zinc-950/80 shadow-xl mx-10 p-2 border border-zinc-700 rounded-lg text-white text-center hover:underline will-change-auto' onClick={() => {
             console.log(theme)
-            if (theme === "light") { setTheme("dark")
-             } else { setTheme("light") }
-          }}><SunMoon></SunMoon><p className='mx-auto'>テーマを切り替える</p>
-          </div>
+            if (theme === "light") {
+              setTheme("dark")
+            } else { setTheme("light") }
+          }}>{theme === "light" ? <Moon /> : <Sun />}<span className='mx-auto'>テーマを切り替える</span>
+          </button>
           <Link className="flex bg-gray-900 dark:bg-zinc-950/80 shadow-xl mx-10 p-2 border border-zinc-700 rounded-lg text-white text-center hover:underline will-change-auto"
             to='https://docs.google.com/forms/d/e/1FAIpQLScPysPRj60-S2v_zmFjrQF6YKlS0Qe200GSO4LnEMsiVbXxYg/viewform?usp=sf_link'>
             <Send />
