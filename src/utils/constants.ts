8fa1a58ca1c373e073e2e56a35511e5515b51dfc
tommy10092525 <tommyxla777@gmail.com>
2025-06-29 01:28:01 +0900

    使用しないURLの記述を削除

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,9 +11,7 @@ const stationNames = {
   "aihara": "相原",
 }
 
-const timetableApi="api"
-const holidayDataAPi="https://holidays-jp.github.io/api/v1/date.json"
 const majorStations=["相原駅西口","法政大学（町田市）","橋本駅北口","法政大学","めじろ台駅","西八王子駅南口","八王子駅南口"]
 
-export {buildings,stationNames,timetableApi,holidayDataAPi,majorStations
+export {buildings,stationNames,majorStations
 }
