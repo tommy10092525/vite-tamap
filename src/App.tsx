db9b1c927e9e4d1626bee2a1b49c6b3f819d543b
tommy10092525 <tommyxla777@gmail.com>
2025-07-06 01:49:53 +0900

    google analyticsの設定を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,50 @@
-// import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "../src/pages/home"
-import Discount from "../src/pages/discount/discount"
-import Fuji from "../src/pages/discount/fuji"
-import Hicheese from "../src/pages/discount/hicheese"
-import Kokuterudo from "./pages/discount/kokuterudo"
-import { ThemeProvider } from "./components/theme-provider"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import Home from "../src/pages/home";
+import Discount from "../src/pages/discount/discount";
+import Fuji from "../src/pages/discount/fuji";
+import Hicheese from "../src/pages/discount/hicheese";
+import Kokuterudo from "./pages/discount/kokuterudo";
+import { ThemeProvider } from "./components/theme-provider";
+
+const TRACKING_ID = "G-4F3PMM48SS";
+if (TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+}
+
+const TrackPageViews = () => {
+  const location = useLocation();
+  useEffect(() => {
+    if (TRACKING_ID) {
+      ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+    }
+  }, [location]);
+
+  return null;
+};
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="tamap-theme">
       <BrowserRouter basename={import.meta.env.PROD ? "/tamap/" : ""}>
+        <TrackPageViews />
         <Routes>
-          <Route path="/" element={
-            <Home />
-          } />
+          <Route
+            path="/"
+            element={
+              <Home />
+            }
+          />
           <Route path="discount" element={<Discount />} />
           <Route path="fuji" element={<Fuji />} />
           <Route path="hicheese" element={<Hicheese />} />
           <Route path="kokuterudo" element={<Kokuterudo />} />
-          <Route path="*" element={404} />
+          <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
