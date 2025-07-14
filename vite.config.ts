0bdecd8b6cf21223047ee70f8cfaafc5f2a0af4d
tommy10092525 <tommyxla777@gmail.com>
2025-07-15 00:18:57 +0900

    robots.txtの設定の追加

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png',"robots.txt"],
       injectRegister:"auto",
       manifest: {
         name: 'たまっぷ',
