357fb82b7af7c6566c196b1cb039019cae3290b5
tommy10092525 <tommyxla777@gmail.com>
2025-07-15 00:17:05 +0900

    祝日の表記

--- src/components/ui/Clock.tsx
+++ src/components/ui/Clock.tsx
@@ -1,20 +1,25 @@
-import { useEffect, useState } from 'react'
-import { getDateString, getTimeString } from '../../utils/timeHandlers'
+// import { useEffect, useState } from 'react'
+import { getDateString, getTimeString, isHoliday } from '../../utils/timeHandlers'
+import holidayData from '../../utils/Holidays.json'
 
-const Clock = () => {
-  const [, setNow] = useState(new Date())
-  useEffect(()=>{
-    setInterval(() => {
-      setNow(new Date())
-    },1000);
-  },[])
+type Props={
+    now: Date
+}
+
+const Clock = ({ now }: Props) => {
+  if(isHoliday({ date: now ,holidayData})) {
+    return (
+      <div className="top-3 left-3 z-10 fixed bg-white/70 dark:bg-black/60 shadow backdrop-blur-sm p-5 rounded-full w-1/3 text-black dark:text-white">
+        <p suppressHydrationWarning={false} className="w-auto h-7 font-medium text-lg text-center">{getDateString(now)}</p>
+        <p suppressHydrationWarning={false} className="w-auto h-7 font-medium text-2xl text-center">{getTimeString(now)}</p>
+        <p className="text-red-500 text-center">{isHoliday({ date: now, holidayData })}</p>
+      </div>
+    )
+  }
   return (
     <div className="top-3 left-3 z-10 fixed bg-white/70 dark:bg-black/60 shadow backdrop-blur-sm p-5 rounded-full w-1/3 text-black dark:text-white">
-      <p suppressHydrationWarning={false} className="w-auto h-7 font-medium text-lg text-center">{getDateString()}</p>
-      {/* <p suppressHydrationWarning={false} className="w-auto h-7 font-mono text-lg text-center">{(() => {
-        return dayIndices[new Date().getDay()]
-      })()}</p> */}
-      <p suppressHydrationWarning={false} className="w-auto h-7 font-medium text-2xl text-center">{getTimeString()}</p>
+      <p suppressHydrationWarning={false} className="w-auto h-7 font-medium text-lg text-center">{getDateString(now)}</p>
+      <p suppressHydrationWarning={false} className="w-auto h-7 font-medium text-2xl text-center">{getTimeString(now)}</p>
     </div>
   )
 }
