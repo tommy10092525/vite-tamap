7349d39f336f2899938027d2d740bee0f32dd8aa
tommy10092525 <154877687+tommy10092525@users.noreply.github.com>
2025-10-16 23:39:46 +0900

    モバイルオーダーバナーコンポーネント化

---
+++ src/components/mobile-order-banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileOrderBanner = () => {
+  return (
+    <div className="bg-gradient-to-br from-yellow-400 to-red-500 shadow-lg font-bold text-3xl p-6 text-center">
+      2025多摩祭に<br />
+      初出店します！！          </div>
+  )
+}
+
+export default MobileOrderBanner
