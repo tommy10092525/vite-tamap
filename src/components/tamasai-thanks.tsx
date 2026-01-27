c4e40d5ab7812620983398b020e937154db5408c
tommy10092525 <154877687+tommy10092525@users.noreply.github.com>
2026-01-27 16:07:58 +0900

    多摩祭ありがとうコンポーネント

--- src/components/tamasai-thanks.tsx
+++ src/components/tamasai-thanks.tsx
@@ -1,6 +1,6 @@
 const TamasaiThanks = () => {
   return (
-    <div className='p-6 bg-linear-to-br from-yellow-400 to-red-500 font-extrabold text-white text-2xl text-center'>
+    <div className='p-6 bg-linear-to-br/oklch from-yellow-400 to-red-500 font-extrabold text-white text-2xl text-center'>
       私たちCODE MATESは
       <br />
       2025多摩祭屋台GPにて優勝しました！！！
