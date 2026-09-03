907f126dd0bb91c6bd04a2d8e5f8a1887c7f2a74
tommy10092525 <154877687+tommy10092525@users.noreply.github.com>
2026-09-04 06:10:50 +0900

    pwaの設定を削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,15 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
-import { VitePWA } from 'vite-plugin-pwa'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  VitePWA({
-    workbox: {
-      maximumFileSizeToCacheInBytes: 5000000
-    },
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', "robots.txt"],
-    injectRegister: "auto",
-    manifest: {
-      name: 'たまっぷ',
-      short_name: 'たまっぷ',
-      description: '多摩キャン生が開発！法大生のためのバス乗り換えアプリ',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'icon_512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'
-        }
-      ]
-    }
-  })
-  ],
+  plugins: [react(), tailwindcss()],
   base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-})
+});
