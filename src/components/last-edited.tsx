a9ff997f078bb72ec7506f346c8e14c748762ae2
tommy10092525 <154877687+tommy10092525@users.noreply.github.com>
2026-09-04 06:15:17 +0900

    最終更新日時を変更

--- src/components/last-edited.tsx
+++ src/components/last-edited.tsx
@@ -2,7 +2,7 @@ const LastEdited = () => {
   return (
     <div className="fixed bottom-4 left-4 text-xs backdrop-blur-md bg-white/10 p-1 rounded">
         最終更新<br />
-        2026年4月29日0時07分
+        2026年9月4日
       </div>
   )
 }
